b6d41df27fc2062d5f1ee18324be096ee1c3096f
sarahZiadah <ziadahsarah@gmail.com>
2025-10-04 21:03:12 +0300

    logic done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(PointsCounter());
 }
 
-class PointsCounter extends StatelessWidget {
-  const PointsCounter({super.key});
+class PointsCounter extends StatefulWidget {
+  @override
+  State<PointsCounter> createState() => _PointsCounterState();
+}
+
+class _PointsCounterState extends State<PointsCounter> {
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +31,11 @@ class PointsCounter extends StatelessWidget {
         body: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top:15.0),
+              padding: const EdgeInsets.only(top: 15.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  teamInfo("Team A"),
+                  teamInfo("Team A", teamAPoints),
                   SizedBox(
                     height: 450,
                     child: VerticalDivider(
@@ -39,13 +44,35 @@ class PointsCounter extends StatelessWidget {
                       indent: 40,
                     ),
                   ),
-                  teamInfo("Team B"),
+                  teamInfo("Team B", teamBPoints),
                 ],
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 140.0),
-              child: SizedBox(width: 170, child: button("Reset")),
+              child: SizedBox(
+                width: 170,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      reset();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 40),
+                      backgroundColor: Colors.orange,
+                      elevation: 3.0,
+                      shadowColor: const Color.fromARGB(255, 213, 213, 213),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          5,
+                        ), // Adjust the radius as needed
+                      ),
+                    ),
+                    child: Text("Reset", style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
@@ -53,11 +80,13 @@ class PointsCounter extends StatelessWidget {
     );
   }
 
-  Widget button(String info) {
+  Widget button(String info, int pointsNum, int team) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 6.0),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          addPoint(pointsNum, team);
+        },
         style: ElevatedButton.styleFrom(
           minimumSize: Size(100, 40),
           backgroundColor: Colors.orange,
@@ -74,17 +103,36 @@ class PointsCounter extends StatelessWidget {
     );
   }
 
-  Widget teamInfo(String teamName) {
-    return 
-      
-       Column(
-        children: [
-          Text(teamName, style: TextStyle(fontSize: 32)),
-          Text("0", style: TextStyle(fontSize: 150)),
-          button("Add 1 Point  "),
-          button("Add 2 Points"),
-          button("Add 3 Points"),
-        ],
+  Widget teamInfo(String teamName, int teamPoints) {
+    return Column(
+      children: [
+        Text(teamName, style: TextStyle(fontSize: 32)),
+        Text('$teamPoints', style: TextStyle(fontSize: 150)),
+        button("Add 1 Point  ", 1, teamName == "Team A" ? 1 : 2),
+        button("Add 2 Points", 2, teamName == "Team A" ? 1 : 2),
+        button("Add 3 Points", 3, teamName == "Team A" ? 1 : 2),
+      ],
     );
+  }
+
+  void addPoint(int pointsNum, int team) {
+    if (team == 1) {
+      teamAPoints += pointsNum;
+      setState(() {
+        
+      });
+    } else {
+      teamBPoints += pointsNum;
+      setState(() {
+        
+      });
+    }
+  }
+
+  void reset() {
+    teamAPoints = teamBPoints = 0;
+    setState(() {
+      
+    });
   }
 }
